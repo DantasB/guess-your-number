777367d65b76fbf9a0f59eaf18ab1b8ddb2ce9c1
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-01-17 14:57:23 -0300

    [First Commit] Adding the Game

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    private int guess;
+
+    [SerializeField] private int min;
+    [SerializeField] private int max;
+    [SerializeField] private Text guessTextComponent;
+
+    private List<int> alreadyGuessedNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        max++;
+
+        guess                   = Random.Range(min, max);
+        guessTextComponent.text = guess.ToString();
+
+        alreadyGuessedNumber = new List<int>
+        {
+            guess
+        };
+    }
+
+    public void OnClickHigher()
+    {
+        GetNewGuess(min = guess, max);
+    }
+
+    public void OnClickLower()
+    {
+        GetNewGuess(min, max = guess);
+    }
+
+    private void GetNewGuess(int min, int max)
+    {
+        GetNotRepeatedGuess();
+        AddGuessToList();
+        guessTextComponent.text = guess.ToString();
+    }
+
+    private void GetNotRepeatedGuess()
+    {
+        guess = Random.Range(min, max);
+        while (alreadyGuessedNumber.Contains(guess) && guess != max)
+        {
+            guess = Random.Range(min, max);
+            Debug.Log(guess);
+        }
+    }
+
+    private void AddGuessToList()
+    {
+        if (!alreadyGuessedNumber.Contains(guess))
+        {
+            alreadyGuessedNumber.Add(guess);
+        }
+    }
+}
